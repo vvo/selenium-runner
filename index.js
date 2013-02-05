d47ecb4231316904971ff5105a5ac9d271196af3
vvo <vincent.voyer@gmail.com>
2013-02-05 12:00:09 +0100

    Remove `res` param from testCallback

--- index.js
+++ index.js
@@ -38,9 +38,8 @@ module.exports = function seleniumRunner (opt, tests, testCb, endCb) {
 
 function launchTest (opt, cb) {
     browse(opt.url, opt.desired, opt.remoteCfg, function(err, browser) {
-        opt.exec(browser, function(err, res) {
+        opt.exec(browser, function(err) {
             var context = opt.url + " on " + JSON.stringify(opt.desired);
-            cb(err, context, res);
         });
     })
 }
